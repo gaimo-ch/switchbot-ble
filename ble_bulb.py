025fbb18cbe30f7ed33b6c479061360995ac4661
gaimo-ch <gaimovski.dev@gmail.com>
2024-06-15 17:13:54 +0100

    🩹 Fix | 変数名の変更をする

--- ble_bulb.py
+++ ble_bulb.py
@@ -2,7 +2,7 @@ from bluepy.btle import Peripheral
 from concurrent.futures import ThreadPoolExecutor
 
 # MACアドレス
-MAC_ADDR = ['']
+MAC_ADDRESS = ['']
 
 # RX characteristic UUID
 RX_UUID = 'cba20002-224d-11e6-9fb8-0002a5d5c51b'
@@ -11,30 +11,30 @@ RX_UUID = 'cba20002-224d-11e6-9fb8-0002a5d5c51b'
 TURN_ON = bytes([0x57, 0x0f, 0x47, 0x01, 0x01])
 TURN_OFF = bytes([0x57, 0x0f, 0x47, 0x01, 0x02])
 
-def control_bulb(mac, command, action):
+def control_bulb(mac_address, command, message):
     '''
     BulbをON/OFFする
 
     Parameters:
         mac (str): MACアドレス
         command (bytes): コマンド
-        action (str): ON/OFF
+        message (str): ON/OFF
     '''
     try:
         # デバイスに接続
-        with Peripheral(mac) as peripheral:
+        with Peripheral(mac_address) as peripheral:
             # RX characteristicを取得
             rx_char = peripheral.getCharacteristics(uuid=RX_UUID)[0]
             # コマンドを送信
             rx_char.write(command, withResponse=True)
-            print(action)
+            print(message)
     except Exception as e:
         print(f'Error: {e}')
 
 if __name__ == '__main__':
     # MAC_ADDRの数だけスレッドを立てて並列処理を行う
-    with ThreadPoolExecutor(max_workers=len(MAC_ADDR)) as executor:
+    with ThreadPoolExecutor(max_workers=len(MAC_ADDRESS)) as executor:
         # Bulbsをオンにする
-        executor.map(lambda mac: control_bulb(mac, TURN_ON, 'ON'), MAC_ADDR)
+        executor.map(lambda mac_address: control_bulb(mac_address, TURN_ON, 'ON'), MAC_ADDRESS)
         # Bulbsをオフにする
-        # executor.map(lambda mac: control_bulb(mac, TURN_OFF, 'OFF'), MAC_ADDR)
+        # executor.map(lambda mac_address: control_bulb(mac_address, TURN_OFF, 'OFF'), MAC_ADDRESS)
--- numpad_bulb_ctrl.py
+++ numpad_bulb_ctrl.py
@@ -3,7 +3,7 @@ from bluepy.btle import Peripheral
 from concurrent.futures import ThreadPoolExecutor
 
 # MACアドレス
-MAC = ['']
+MAC_ADDRESS = ['']
 
 # RX characteristic UUID
 RX_UUID = 'cba20002-224d-11e6-9fb8-0002a5d5c51b'
@@ -12,18 +12,18 @@ RX_UUID = 'cba20002-224d-11e6-9fb8-0002a5d5c51b'
 TURN_ON = bytes([0x57, 0x0f, 0x47, 0x01, 0x01])
 TURN_OFF = bytes([0x57, 0x0f, 0x47, 0x01, 0x02])
 
-def control_bulb(mac, command, action):
+def control_bulb(mac_address, command, message):
     '''
     BulbをON/OFFする
 
     Parameters:
         mac (str): MACアドレス
         command (bytes): コマンド
-        action (str): ON/OFF
+        message (str): ON/OFF
     '''
     try:
         # デバイスに接続
-        with Peripheral(mac) as peripheral:
+        with Peripheral(mac_address) as peripheral:
             # RX characteristicを取得
             rx_char = peripheral.getCharacteristics(uuid=RX_UUID)[0]
             # コマンドを送信
@@ -36,11 +36,11 @@ def main(stdscr):
         while True:
             key = stdscr.getch()
             if key == ord('0'):  # 0キー
-                with ThreadPoolExecutor(max_workers=len(MAC)) as executor:
-                    executor.map(lambda mac: control_bulb(mac, TURN_OFF, 'OFF'), MAC)
+                with ThreadPoolExecutor(max_workers=len(MAC_ADDRESS)) as executor:
+                    executor.map(lambda mac_address: control_bulb(mac_address, TURN_OFF, 'OFF'), MAC_ADDRESS)
             elif key == curses.KEY_ENTER or key == 10:  # Enterキー
-                with ThreadPoolExecutor(max_workers=len(MAC)) as executor:
-                    executor.map(lambda mac: control_bulb(mac, TURN_ON, 'ON'), MAC)
+                with ThreadPoolExecutor(max_workers=len(MAC_ADDRESS)) as executor:
+                    executor.map(lambda mac_address: control_bulb(mac_address, TURN_ON, 'ON'), MAC_ADDRESS)
             else:
                 print('Detected unrecognized input!!')
     except KeyboardInterrupt:
