530a9e629a5c5827a32d6516f198e91718be192c
gaimo-ch <gaimovski.dev@gmail.com>
2024-06-20 16:26:33 +0100

    🚧 Add | 色を追加する

--- ble_bulb.py
+++ ble_bulb.py
@@ -7,9 +7,11 @@ MAC_ADDRESS = ['']
 # RX characteristic UUID
 RX_UUID = 'cba20002-224d-11e6-9fb8-0002a5d5c51b'
 
-# REQコマンド (バルブをオン/オフにする)
-TURN_ON = bytes([0x57, 0x0f, 0x47, 0x01, 0x01])
-TURN_OFF = bytes([0x57, 0x0f, 0x47, 0x01, 0x02])
+# REQコマンド
+TURN_ON = bytes([0x57, 0x01, 0x01])
+TURN_OFF = bytes([0x57, 0x01, 0x02])
+WHITE = bytes([0x57, 0x0F, 0x47, 0x01, 0x13, 0x64, 0x19])
+ORANGE = bytes([0x57, 0x0F, 0x47, 0x01, 0x12, 0x64, 0xFC, 0x60, 0x12])
 
 def control_bulb(mac_address, command, message):
     '''
@@ -27,7 +29,7 @@ def control_bulb(mac_address, command, message):
             rx_char = peripheral.getCharacteristics(uuid=RX_UUID)[0]
             # コマンドを送信
             rx_char.write(command, withResponse=True)
-            print(message)
+            print((f'Bulb is {message}'))
     except Exception as e:
         print(f'Error: {e}')
 
@@ -38,3 +40,8 @@ if __name__ == '__main__':
         executor.map(lambda mac_address: control_bulb(mac_address, TURN_ON, 'ON'), MAC_ADDRESS)
         # Bulbsをオフにする
         # executor.map(lambda mac_address: control_bulb(mac_address, TURN_OFF, 'OFF'), MAC_ADDRESS)
+        # Bulbsを白色にする
+        # executor.map(lambda mac_address: control_bulb(mac_address, WHITE, 'WHITE'), MAC_ADDRESS)
+        # Bulbsをオレンジ色にする
+        # executor.map(lambda mac_address: control_bulb(mac_address, ORANGE, 'ORANGE'), MAC_ADDRESS)
+
--- numpad_bulb_ctrl.py
+++ numpad_bulb_ctrl.py
@@ -8,9 +8,11 @@ MAC_ADDRESS = ['']
 # RX characteristic UUID
 RX_UUID = 'cba20002-224d-11e6-9fb8-0002a5d5c51b'
 
-# REQコマンド (バルブをオン/オフにする)
-TURN_ON = bytes([0x57, 0x0f, 0x47, 0x01, 0x01])
-TURN_OFF = bytes([0x57, 0x0f, 0x47, 0x01, 0x02])
+# REQコマンド
+TURN_ON = bytes([0x57, 0x01, 0x01])
+TURN_OFF = bytes([0x57, 0x01, 0x02])
+WHITE = bytes([0x57, 0x0F, 0x47, 0x01, 0x13, 0x64, 0x19])
+ORANGE = bytes([0x57, 0x0F, 0x47, 0x01, 0x12, 0x64, 0xFC, 0x60, 0x12])
 
 def control_bulb(mac_address, command, message):
     '''
@@ -28,6 +30,7 @@ def control_bulb(mac_address, command, message):
             rx_char = peripheral.getCharacteristics(uuid=RX_UUID)[0]
             # コマンドを送信
             rx_char.write(command, withResponse=True)
+            print(f'Bulb is {message}.')
     except Exception as e:
         print(f'Error: {e}')
 
@@ -41,6 +44,12 @@ def main(stdscr):
             elif key == curses.KEY_ENTER or key == 10:  # Enterキー
                 with ThreadPoolExecutor(max_workers=len(MAC_ADDRESS)) as executor:
                     executor.map(lambda mac_address: control_bulb(mac_address, TURN_ON, 'ON'), MAC_ADDRESS)
+            elif key == ord('/'): # /キー
+                with ThreadPoolExecutor(max_workers=len(MAC_ADDRESS)) as executor:
+                    executor.map(lambda mac_address: control_bulb(mac_address, WHITE, 'WHITE'), MAC_ADDRESS)
+            elif key == 9:  # tabキー
+                with ThreadPoolExecutor(max_workers=len(MAC_ADDRESS)) as executor:
+                    executor.map(lambda mac_address: control_bulb(mac_address, ORANGE, 'ORANGE'), MAC_ADDRESS)
             else:
                 print('Detected unrecognized input!!')
     except KeyboardInterrupt:
